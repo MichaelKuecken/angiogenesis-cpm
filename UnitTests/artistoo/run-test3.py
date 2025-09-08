22449974251c14a41fa2c5f3ddf271ead5f99f47
Inge Wortel <ingewortel@gmail.com>
2025-09-08 19:54:48 +0200

    modify wrapper for test 3 to allow alternative script

--- UnitTests/artistoo/run-test3.py
+++ UnitTests/artistoo/run-test3.py
@@ -7,7 +7,46 @@ import glob
 import numpy as np
 import pandas as pd
 from Naked.toolshed.shell import execute_js, execute
+import argparse
 
+class ArgParser(argparse.ArgumentParser):
+    
+	def __init__(self):
+		super().__init__(description='Execute specification with Artistoo')
+
+		self.add_argument('-s', '--script',
+                          type=str,
+                          required=False,
+                          dest='script_path',
+                          default = "src/angiogenesis-test3.js",
+                          help='simulation script')
+
+		self.add_argument('-o', '--out',
+                          type=str,
+                          required=False,
+                          dest='out_path',
+                          default="output/test3/artistoo-test3.csv",
+                          help='Path to output csv')
+
+
+		self.parsed_args = self.parse_args()
+        
+	@property
+	def script_path(self):
+		return f'{os.getcwd()}/{self.parsed_args.script_path}'
+
+	@property
+	def out_path(self):
+		return f'{os.getcwd()}/{self.parsed_args.out_path}'
+
+	@property
+	def kwargs(self):
+		return dict(
+			script=self.script_path,
+			out=self.out_path
+		)
+
+args = ArgParser().kwargs
 
 """ 
 	=========================== Functions ===========================
@@ -17,9 +56,9 @@ from Naked.toolshed.shell import execute_js, execute
 """
 def run_node() :
 
-	argString = " > output/test3/artistoo-test3.csv" 
+	argString = " > " + args['out']
 	#print(script + " " + argString)
-	success = execute_js("src/angiogenesis-test3.js", argString ) #execute_js(args['script'], argString )
+	success = execute_js(args['script'], argString ) #execute_js(args['script'], argString )
 	if success:
 		pass
 	else:
